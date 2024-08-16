ba87b6721340fbedd9faa5d8db504f28100bbc57
Jaco Cabral <larbacocaj@hotmail.com>
2024-08-16 20:26:01 -0300

    add jwt base

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+const tokenMaker = require('jsonwebtoken')
+
 const express = require('express');
+
+const bodyParser = require('body-parser');
+
 const app = express();
 
-// Rota raiz que retorna "Olá, Mundo!"
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    res.send('AAAAAA!');
-});
+    const generateToken = () => {
+        const payload = {
+            message: 'esse é o payload do seu token heheh'
+        };
+    
+        return tokenMaker.sign(payload, process.env.CHAVE, { expiresIn: '1d' });
+    };
+
+    const token = generateToken();
+
+    return res.status(200).json({token})
+})
+
+app.post('/', (req, res) => {
+    const { token } = req.body
+
+    const payload = tokenMaker.verify(token, process.env.CHAVE, (err, payload) => {
+        if (err) {
+            return res.status(403).json({ message: 'ERROR: Token invalido' });
+        }
+
+        return payload;
+    });
+
+    return res.status(200).json({ message: payload.message });
+})
 
 // Configura o servidor para escutar na porta 3000
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+app.listen(port, () => {
+    console.log('Servidor rodando na porta:'+port);
 });
